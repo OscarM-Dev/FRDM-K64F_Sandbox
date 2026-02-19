bb3bdf2fe312c9c5bfb5e3a5e783d213a1e74925
Oscar Mercado <omercadorico@gmail.com>
2026-02-18 20:35:56 -0600

    Adding chacha20 library with encryption and decrytion functions

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
 /**
  * @file main.c
  * @author Oscar Mercado, omercadorico@gmail.com
- * @brief This file contains a simple example of the use of mbedTLS library to generate pseudo random numbers 
- * in a buffer and print those numbers via serial terminal.
+ * @brief This file contains a simple example of the use of the mbedTLS library to encrypt and decrypt a message
+ * using ChaCha20.
  * 
  */
 /*******************************************************************************
@@ -14,6 +14,7 @@
 #include "mbedtls/platform.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
+#include "mbedtls/chacha20.h"
 #include "fsl_debug_console.h"
 #include "fsl_clock.h"
 #include "pin_mux.h"
@@ -30,7 +31,8 @@
  * Private functions prototypes
  ******************************************************************************/
 static int myrand( void *rng_state, uint8_t *output, size_t len );
-static void generate_random_numbers( void );
+static int8_t chacha20_encrypt( uint8_t *key, uint8_t *nonce, uint32_t counter, size_t length, uint8_t *input, uint8_t *output );
+static int8_t chacha20_decrypt( uint8_t *key, uint8_t *nonce, uint32_t counter, size_t length, uint8_t *input, uint8_t *output );
 
 /*******************************************************************************
  * Private functions definition
@@ -67,44 +69,56 @@ static int myrand( void *rng_state, uint8_t *output, size_t len )
 }
 
 /**
- * @brief This function generates 15 pseudo random numbers with mbedTLS 
- * and prints them via serial terminal.
+ * @brief This function encrypts a given buffer with ChaCha20 using mbedTLS library.
  * 
+ * @param key Pointer to encryption key.
+ * @param nonce Pointer to nonce.
+ * @param counter Initial counter value to use for keystream.
+ * @param length Number of bytes to encrypt ( input buffer size ).
+ * @param input Pointer to input buffer ( plain text ).
+ * @param output Pointer to output buffer ( cypher text ).
+ * @retval Operation result.
  */
-void generate_random_numbers( void )
+int8_t chacha20_encrypt( uint8_t *key, uint8_t *nonce, uint32_t counter, size_t length, uint8_t *input, uint8_t *output )
 {
-    mbedtls_ctr_drbg_context ctrDrbg;
-    uint32_t number;
+    int8_t result = -1;
+    mbedtls_chacha20_context ctx;
 
-    //Generating seed for RAND function.
-    srand( RAND_SEED );
-
-    //Initializing drbg and generating seed with rand source entropy.
-    mbedtls_ctr_drbg_init( &ctrDrbg );
-
-    if ( mbedtls_ctr_drbg_seed( &ctrDrbg, myrand, NULL, NULL, 0 ) != 0 )
+    if ( key != NULL && nonce != NULL && input != NULL && output != NULL )
     {
-        PRINTF( "Seed error\r\n" );
-        return;
-    }
+        //Initialize chacha20 context struct.
+        mbedtls_chacha20_init( &ctx );
 
-    PRINTF( "%d pseudo random numbers:\r\n", NUM_RANDOM );
+        //Register input matrix for keystrem generation.
+        mbedtls_chacha20_setkey( &ctx, key );
+        mbedtls_chacha20_starts( &ctx, nonce, counter );
 
-    //Generating random numbers.
-    for ( uint8_t i = 0; i < NUM_RANDOM; i++ )
-    {
-        if ( mbedtls_ctr_drbg_random( &ctrDrbg, ( uint8_t * ) &number, sizeof( number ) ) != 0 )
-        {
-            PRINTF( "Random error\r\n" );
-            break;
-        }
+        //Generate n 64byte keystreams and xor them with input buffer.
+        result = mbedtls_chacha20_update( &ctx, length, input, output );
 
-        PRINTF( "Pseudo random number %d\t%lu\r\n", i + 1, ( uint32_t ) number );
-    }
+        //Free chacha20 context struct.
+        mbedtls_chacha20_free( &ctx );  
+    }   
 
-    //Free resources.
-    mbedtls_ctr_drbg_free( &ctrDrbg );
+    return result;
 }
+
+/**
+ * @brief This function decrypts a given buffer with ChaCha20 using mbedTLS library.
+ * 
+ * @param key Pointer to encryption key.
+ * @param nonce Pointer to nonce.
+ * @param counter Initial counter value to use for keystream.
+ * @param length Number of bytes to decrypt ( input buffer size ).
+ * @param input Pointer to input buffer ( cypher text ).
+ * @param output Pointer to output buffer ( plain text ).
+ * @retval Operation result.
+ */
+int8_t chacha20_decrypt( uint8_t *key, uint8_t *nonce, uint32_t counter, size_t length, uint8_t *input, uint8_t *output )
+{
+    return chacha20_encrypt( key, nonce, counter, input, output, length );
+}
+
 
 /*******************************************************************************
  * Private functions definition
@@ -119,8 +133,7 @@ int main(void)
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
 
-    PRINTF( "\r\nMbedTLS example start.\r\n" );
-    generate_random_numbers();
+    PRINTF( "\r\nMbedTLS chacha20 example start.\r\n" );
 
     while ( 1 )
     {
